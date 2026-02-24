4742bdc82d96862f6bd22093170f73364a292d89
Kevin Yang <kev4238@gmail.com>
2026-02-23 21:52:55 -0500

    Added assets folder

--- Backend/PublicServiceRequestBackend.Tests/obj/Debug/net10.0/PublicServiceRequestBackend.Tests.AssemblyInfo.cs
+++ Backend/PublicServiceRequestBackend.Tests/obj/Debug/net10.0/PublicServiceRequestBackend.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublicServiceRequestBackend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73861171940512bdc881b4fbe0a2e802b8a0dbab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349fa13c18ece3e960fb0695f631fe38835cc0de")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublicServiceRequestBackend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublicServiceRequestBackend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
